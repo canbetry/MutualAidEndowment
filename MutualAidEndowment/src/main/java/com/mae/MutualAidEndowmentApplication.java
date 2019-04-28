6e4e64e473e9ecbff2587e43db4caab14ff578ad
canbetry <canbetry@163.com>
2019-04-28 17:33:48 +0800

    初始demo

---
+++ MutualAidEndowment/src/main/java/com/mae/MutualAidEndowmentApplication.java
@@ -0,0 +1,13 @@
+package com.mae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualAidEndowmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualAidEndowmentApplication.class, args);
+	}
+
+}
