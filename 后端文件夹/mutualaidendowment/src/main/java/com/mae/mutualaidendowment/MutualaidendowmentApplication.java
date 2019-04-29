0dbc65b7c24b4106679d8f88b80c85fa5b4001fd
canbetry <canbetry@163.com>
2019-04-29 17:38:32 +0800

    使用idea创建项目模板

---
+++ 后端文件夹/mutualaidendowment/src/main/java/com/mae/mutualaidendowment/MutualaidendowmentApplication.java
@@ -0,0 +1,13 @@
+package com.mae.mutualaidendowment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualaidendowmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualaidendowmentApplication.class, args);
+    }
+
+}
